65c224c690845466f07db5804f98f97326e86282
Ben <peng-chen@live.com>
2025-07-02 18:34:15 +0800

    update build

--- test/build.sh
+++ test/build.sh
@@ -1,7 +1,8 @@
-g++ -std=c++11 -O3 -o shmq_send shmq_send.cc -lrt
-g++ -std=c++11 -O3 -o shmq_recv shmq_recv.cc -lrt
+mkdir -p bin
+g++ -std=c++11 -O3 -o bin/shmq_send shmq_send.cc -lrt
+g++ -std=c++11 -O3 -o bin/shmq_recv shmq_recv.cc -lrt
 
-g++ -std=c++11 -O3 -o multhread_q multhread_q.cc -lrt -pthread
+g++ -std=c++11 -O3 -o bin/multhread_q multhread_q.cc -lrt -pthread
 
-g++ -std=c++11 -O3 -o multhread_varq multhread_varq.cc -lrt -pthread
+g++ -std=c++11 -O3 -o bin/multhread_varq multhread_varq.cc -lrt -pthread
 
